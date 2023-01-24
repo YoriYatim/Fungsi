b880a8c8fd36880dceb7b2b85ac65627af4c4df3
YoriYatim <122761708+YoriYatim@users.noreply.github.com>
2023-01-24 14:23:24 +0700

    Add files via upload

---
+++ fungsi.py
@@ -0,0 +1,11 @@
+print("================")
+print("=              =")
+print("=   INI YORI   =")
+print("=              =")
+print("================")
+def salam():
+     print ("hello, selamat pagi")
+
+salam()
+salam()
+salam()
